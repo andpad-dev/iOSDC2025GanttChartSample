7a392b0130dc93aacefbd94bf60822c57be1b8af
Yusaku Nishi <yusaku.nishi@andpad.co.jp>
2025-09-05 11:28:32 +0900

    Initial Commit

---
+++ iOSDC2025GanttChartSample/iOSDC2025GanttChartSampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  iOSDC2025GanttChartSampleApp.swift
+//  iOSDC2025GanttChartSample
+//
+//  Created by 西悠作 on 2025/09/05.
+//
+
+import SwiftUI
+
+@main
+struct iOSDC2025GanttChartSampleApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
