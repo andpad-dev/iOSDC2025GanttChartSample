29c1ae588cd7220e2c9cee616c434577d47495c9
Yusaku Nishi <yusaku.nishi@andpad.co.jp>
2025-09-05 11:43:57 +0900

    update: show gantt chart

--- iOSDC2025GanttChartSample/ContentView.swift
+++ iOSDC2025GanttChartSample/ContentView.swift
@@ -10,13 +10,16 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            GanttChart()
+                .ignoresSafeArea()
+                .navigationTitle("ガントチャート")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbarBackgroundVisibility(
+                    .visible,
+                    for: .navigationBar
+                )
         }
-        .padding()
     }
 }
 
