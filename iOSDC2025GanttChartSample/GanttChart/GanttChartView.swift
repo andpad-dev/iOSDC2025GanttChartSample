fd2f8598b4fc8690e135a0d2c243d641d85d4379
Yusaku Nishi <yusaku.nishi@andpad.co.jp>
2025-09-05 11:48:57 +0900

    add: collectionView

--- iOSDC2025GanttChartSample/GanttChart/GanttChartView.swift
+++ iOSDC2025GanttChartSample/GanttChart/GanttChartView.swift
@@ -10,6 +10,18 @@ import UIKit
 
 final class GanttChartView: UIView {
     
+    private let collectionView: UICollectionView = {
+        // TODO: Replace with GanttChartViewLayout
+        let layout = UICollectionViewCompositionalLayout.list(
+            using: .init(appearance: .insetGrouped)
+        )
+        let collectionView = UICollectionView(
+            frame: .zero,
+            collectionViewLayout: layout
+        )
+        return collectionView
+    }()
+    
     // MARK: - Initializers
     
     override init(frame: CGRect) {
@@ -24,6 +36,21 @@ final class GanttChartView: UIView {
     
     private func setUpViews() {
         backgroundColor = .systemBackground
+        
+        // Subviews
+        addSubview(collectionView)
+        
+        // Layout
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate(
+            [
+                // collectionView.edges == self.edges
+                collectionView.topAnchor.constraint(equalTo: topAnchor),
+                collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+                collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+                collectionView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            ]
+        )
     }
 }
 
