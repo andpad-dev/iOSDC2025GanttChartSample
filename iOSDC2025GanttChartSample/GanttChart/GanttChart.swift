8b500ac46e68f2fef8e6ce3968fca3b575b18a2a
Yusaku Nishi <yusaku.nishi@andpad.co.jp>
2025-09-05 11:43:30 +0900

    add: GanttChart for SwiftUI

---
+++ iOSDC2025GanttChartSample/GanttChart/GanttChart.swift
@@ -0,0 +1,19 @@
+//
+//  GanttChart.swift
+//  iOSDC2025GanttChartSample
+//
+//  Created by 西悠作 on 2025/09/05.
+//
+
+import SwiftUI
+
+struct GanttChart: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> GanttChartView {
+        GanttChartView()
+    }
+    
+    func updateUIView(_ uiView: GanttChartView, context: Context) {
+        // NOP
+    }
+}
