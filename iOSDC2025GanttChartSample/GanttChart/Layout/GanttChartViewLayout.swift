099cd267cda6d5132566125a0c5b976d63e1edba
Yusaku Nishi <yusaku.nishi@andpad.co.jp>
2025-09-08 19:38:43 +0900

    add: GanttChartViewLayout

---
+++ iOSDC2025GanttChartSample/GanttChart/Layout/GanttChartViewLayout.swift
@@ -0,0 +1,17 @@
+//
+//  GanttChartViewLayout.swift
+//  iOSDC2025GanttChartSample
+//
+//  Created by 西悠作 on 2025/09/08.
+//
+
+import SwiftUI
+import UIKit
+
+final class GanttChartViewLayout: UICollectionViewLayout {
+    
+}
+
+#Preview {
+    ContentView()
+}
