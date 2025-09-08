6d95dccd31c281813fc443e3b874448d5064d7b4
Yusaku Nishi <yusaku.nishi@andpad.co.jp>
2025-09-08 20:19:33 +0900

    add: workItemGroup(with:), workItem(with:)

--- iOSDC2025GanttChartSample/GanttChart/GanttChart.swift
+++ iOSDC2025GanttChartSample/GanttChart/GanttChart.swift
@@ -12,14 +12,44 @@ final class GanttChartState {
     
     var loadingResult: Result<[WorkItemGroup], any Error>?
     
+    /*
+     NOTE:
+     Cache each elements in the form of Dictionary keyed by their ID.
+     These are used to efficiently retrieve elements from the IDs managed by the diffable data source.
+     */
+    private var cachedWorkItemGroups: [WorkItemGroup.ID: WorkItemGroup] = [:]
+    private var cachedWorkItems: [WorkItem.ID: WorkItem] = [:]
+    
+    // MARK: - Methods
+    
     func refresh() async {
         loadingResult = nil
         do {
             try await Task.sleep(for: .milliseconds(500))
-            loadingResult = .success(WorkItemGroup.samples)
+            let workItemGroups = WorkItemGroup.samples
+            
+            // Cache
+            for group in workItemGroups {
+                cachedWorkItemGroups[group.id] = group
+                for item in group.children {
+                    cachedWorkItems[item.id] = item
+                }
+            }
+            
+            loadingResult = .success(workItemGroups)
         } catch {
             loadingResult = .failure(error)
+            cachedWorkItemGroups.removeAll()
+            cachedWorkItems.removeAll()
         }
+    }
+    
+    func workItemGroup(with id: WorkItemGroup.ID) -> WorkItemGroup? {
+        cachedWorkItemGroups[id]
+    }
+    
+    func workItem(with id: WorkItem.ID) -> WorkItem? {
+        cachedWorkItems[id]
     }
 }
 
