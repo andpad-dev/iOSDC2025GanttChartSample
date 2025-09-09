f0f85387bdd9e6614dd25bdd76a65f035266a531
Yusaku Nishi <yusaku.nishi@andpad.co.jp>
2025-09-09 19:48:14 +0900

    add: GanttChartWorkItemGroupHeaderView

---
+++ iOSDC2025GanttChartSample/GanttChart/Subviews/ReusableViews/GanttChartReusableViews.swift
@@ -0,0 +1,57 @@
+//
+//  GanttChartReusableViews.swift
+//  iOSDC2025GanttChartSample
+//
+//  Created by 西悠作 on 2025/09/09.
+//
+
+import SwiftUI
+import UIKit
+
+class GanttChartReusableView: UICollectionReusableView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUpViews()
+    }
+    
+    func setUpViews() {}
+}
+
+final class GanttChartWorkItemGroupHeaderView: GanttChartReusableView {
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .headline)
+        return label
+    }()
+    
+    override func setUpViews() {
+        backgroundColor = .secondarySystemBackground
+        
+        // Subviews
+        addSubview(titleLabel)
+        
+        // Layout
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: topAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8),
+            titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+    }
+    
+    func configure(workItemGroup: WorkItemGroup) {
+        titleLabel.text = workItemGroup.name
+    }
+}
+
+#Preview {
+    ContentView()
+}
